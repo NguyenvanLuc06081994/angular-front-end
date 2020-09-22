a9aa6b058f6b8b393b4b1b2da01008662a652035
quangbui205 <bmq205999@gmail.com>
2020-09-22 16:33:44 +0700

    form checkout book house

--- src/app/houses/checkout/checkout.component.ts
+++ src/app/houses/checkout/checkout.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {HouseService} from '../../services/house.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CustomerService} from '../../services/customer.service';
+import {BillService} from "../../services/bill.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-checkout',
@@ -29,15 +32,38 @@ export class CheckoutComponent implements OnInit {
     username: ''
   };
 
+  bill = {
+    totalPrice: '',
+    checkIn: '',
+    checkOut: '',
+    status: '',
+    order: '',
+    description: '',
+    customer_id: '',
+    house_id: ''
+  };
+
+  formCheckout: FormGroup;
+  userLogin;
+
   constructor(private houseService: HouseService,
               private router: Router,
+              private route: ActivatedRoute,
               private customerService: CustomerService,
-              private route: ActivatedRoute) {
+              private billService: BillService,
+              private fb: FormBuilder,
+              private authService: AuthService) {
   }
 
   id = +this.route.snapshot.paramMap.get('id');
 
   ngOnInit(): void {
+    this.formCheckout = this.fb.group({
+      checkIn: [''],
+      checkOut: [''],
+      order: [''],
+      description: ['']
+    });
     this.getHouse();
   }
 
@@ -52,6 +78,37 @@ export class CheckoutComponent implements OnInit {
       });
     });
 
+  }
+
+  addBill(): any {
+    console.log(this.formCheckout.value);
+    this.userLogin = this.authService.getUserLogin();
+    console.log(this.formCheckout.value.checkIn);
+    this.bill.checkIn = this.formCheckout.value.checkIn;
+    this.bill.checkOut = this.formCheckout.value.checkOut;
+    this.bill.order = this.formCheckout.value.order;
+    this.bill.customer_id = this.userLogin.id;
+    // @ts-ignore
+    this.bill.house_id = this.house.id;
+    const checkIn = new Date(`"${this.formCheckout.value.checkIn}"`);
+    const getDateCheckIn = checkIn.getDate();
+    const checkOut = new Date(`"${this.formCheckout.value.checkOut}"`);
+    const getDateCheckOut = checkOut.getDate();
+    const result = getDateCheckOut - getDateCheckIn;
+    // @ts-ignore
+    this.bill.totalPrice = result * this.house.price;
+    console.log(this.bill);
+    this.bill.status = 'pending';
+    this.bill.order = this.formCheckout.value.order;
+    this.bill.description = this.formCheckout.value.description;
+    this.house.status = 'dang cho thue';
+    console.log(this.bill);
+    this.billService.addBill(this.bill).subscribe(data => {
+    });
+    console.log(this.house);
+    this.houseService.updateStatus(+this.house.id, this.house).subscribe(page => {
+      this.router.navigate(['/home']);
+    });
   }
 
 }

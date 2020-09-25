0796cba6d028924b6f633524dff93402a8dd5e52
quangbui205 <bmq205999@gmail.com>
2020-09-25 11:34:01 +0700

    view bill order old

--- src/app/bills/b-old/b-old.component.ts
+++ src/app/bills/b-old/b-old.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {BillService} from "../../services/bill.service";
+import {HouseService} from "../../services/house.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+import {CustomerService} from "../../services/customer.service";
+import {IBill} from "../../interfaces/ibill";
 
 @Component({
   selector: 'app-b-old',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BOldComponent implements OnInit {
 
-  constructor() { }
+  userLogin;
+  billOder;
+  billOderOld: IBill[] =[];
+
+  constructor(private billService: BillService,
+              private houseService: HouseService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private authService: AuthService,
+              private customerService: CustomerService) {
+
+  }
+
+  id = +this.route.snapshot.paramMap.get('id');
 
   ngOnInit(): void {
+    this.userLogin = this.authService.getUserLogin();
+    this.billService.getBillByUserId(this.userLogin.id).subscribe(bills=>{
+      this.billOder = bills;
+      for(let i =0; i<this.billOder.length;i++)
+      {
+        if(this.billOder[i].status == 'done')
+        {
+          this.billOderOld.push(this.billOder[i]);
+        }
+      }
+    });
   }
 
 }
